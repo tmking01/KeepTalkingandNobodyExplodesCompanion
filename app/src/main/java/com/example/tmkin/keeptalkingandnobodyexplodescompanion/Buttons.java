1e17ae6ecd5bb25b82aa682ba8974e27ce345ec8
tmking01 <tmking01@comcast.net>
2017-06-22 12:58:02 -0500

    Almost done with the wires screen

--- app/src/main/java/com/example/tmkin/keeptalkingandnobodyexplodescompanion/Buttons.java
+++ app/src/main/java/com/example/tmkin/keeptalkingandnobodyexplodescompanion/Buttons.java
@@ -19,56 +19,74 @@ public class Buttons extends AppCompatActivity {
 
     public void btnAbort(View v){
         abort = "A";
-        Button myButtonone = (Button) findViewById(R.id.btnAbort);
+        Button myButtonone = (Button) findViewById(R.id.btnBlue);
         myButtonone.setVisibility(View.VISIBLE);
-        Button myButtontwo = (Button) findViewById(R.id.btnDetonate);
+        Button myButtontwo = (Button) findViewById(R.id.btnYellow);
         myButtontwo.setVisibility(View.VISIBLE);
-        Button myButtonthree = (Button) findViewById(R.id.btnHold);
+        Button myButtonthree = (Button) findViewById(R.id.btnWhite);
         myButtonthree.setVisibility(View.VISIBLE);
-        Button myButtonfour = (Button) findViewById(R.id.btnOther);
+        Button myButtonfour = (Button) findViewById(R.id.btnOtherColor);
         myButtonfour.setVisibility(View.VISIBLE);
         TextView mylabelone = (TextView) findViewById(R.id.lblStrip);
         mylabelone.setVisibility(View.VISIBLE);
     }
     public void btnDetonate(View v){
         abort = "D";
-        Button myButtonone = (Button) findViewById(R.id.btnAbort);
-        Button myButtontwo = (Button) findViewById(R.id.btnDetonate);
-        Button myButtonthree = (Button) findViewById(R.id.btnHold);
-        Button myButtonfour = (Button) findViewById(R.id.btnOther);
-        TextView mylabelone = (TextView) findViewById(R.id.lblStrip);
+        Button myButtonone = (Button) findViewById(R.id.btnBlue);
         myButtonone.setVisibility(View.VISIBLE);
+        Button myButtontwo = (Button) findViewById(R.id.btnYellow);
         myButtontwo.setVisibility(View.VISIBLE);
+        Button myButtonthree = (Button) findViewById(R.id.btnWhite);
         myButtonthree.setVisibility(View.VISIBLE);
+        Button myButtonfour = (Button) findViewById(R.id.btnOtherColor);
         myButtonfour.setVisibility(View.VISIBLE);
+        TextView mylabelone = (TextView) findViewById(R.id.lblStrip);
         mylabelone.setVisibility(View.VISIBLE);
     }
     public void btnHold(View v){
         abort = "H";
-        Button myButtonone = (Button) findViewById(R.id.btnAbort);
-        Button myButtontwo = (Button) findViewById(R.id.btnDetonate);
-        Button myButtonthree = (Button) findViewById(R.id.btnHold);
-        Button myButtonfour = (Button) findViewById(R.id.btnOther);
-        TextView mylabelone = (TextView) findViewById(R.id.lblStrip);
+        Button myButtonone = (Button) findViewById(R.id.btnBlue);
         myButtonone.setVisibility(View.VISIBLE);
+        Button myButtontwo = (Button) findViewById(R.id.btnYellow);
         myButtontwo.setVisibility(View.VISIBLE);
+        Button myButtonthree = (Button) findViewById(R.id.btnWhite);
         myButtonthree.setVisibility(View.VISIBLE);
+        Button myButtonfour = (Button) findViewById(R.id.btnOtherColor);
         myButtonfour.setVisibility(View.VISIBLE);
+        TextView mylabelone = (TextView) findViewById(R.id.lblStrip);
         mylabelone.setVisibility(View.VISIBLE);
     }
     public void btnOther(View v){
         abort = "O";
-        Button myButtonone = (Button) findViewById(R.id.btnAbort);
-        Button myButtontwo = (Button) findViewById(R.id.btnDetonate);
-        Button myButtonthree = (Button) findViewById(R.id.btnHold);
-        Button myButtonfour = (Button) findViewById(R.id.btnOther);
-        TextView mylabelone = (TextView) findViewById(R.id.lblStrip);
+        Button myButtonone = (Button) findViewById(R.id.btnBlue);
         myButtonone.setVisibility(View.VISIBLE);
+        Button myButtontwo = (Button) findViewById(R.id.btnYellow);
         myButtontwo.setVisibility(View.VISIBLE);
+        Button myButtonthree = (Button) findViewById(R.id.btnWhite);
         myButtonthree.setVisibility(View.VISIBLE);
+        Button myButtonfour = (Button) findViewById(R.id.btnOtherColor);
         myButtonfour.setVisibility(View.VISIBLE);
+        TextView mylabelone = (TextView) findViewById(R.id.lblStrip);
         mylabelone.setVisibility(View.VISIBLE);
     }
-
-
+    public void btnBlue(View v){
+        TextView mylabelone = (TextView) findViewById(R.id.lblAnswer);
+        mylabelone.setVisibility(View.VISIBLE);
+        mylabelone.setText("Blue");
+    }
+    public void btnYellow(View v){
+        TextView mylabelone = (TextView) findViewById(R.id.lblAnswer);
+        mylabelone.setVisibility(View.VISIBLE);
+        mylabelone.setText("Yellow");
+    }
+    public void btnWhite(View v){
+        TextView mylabelone = (TextView) findViewById(R.id.lblAnswer);
+        mylabelone.setVisibility(View.VISIBLE);
+        mylabelone.setText("White");
+    }
+    public void btnOtherColor(View v){
+        TextView mylabelone = (TextView) findViewById(R.id.lblAnswer);
+        mylabelone.setVisibility(View.VISIBLE);
+        mylabelone.setText("Other");
+    }
 }

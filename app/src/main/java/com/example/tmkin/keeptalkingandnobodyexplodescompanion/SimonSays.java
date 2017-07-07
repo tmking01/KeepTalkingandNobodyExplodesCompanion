f2f5636da27fd8d137fc78556ddd255e79944b1e
tmking01 <tmking01@comcast.net>
2017-07-07 16:29:11 -0500

    updates

--- app/src/main/java/com/example/tmkin/keeptalkingandnobodyexplodescompanion/SimonSays.java
+++ app/src/main/java/com/example/tmkin/keeptalkingandnobodyexplodescompanion/SimonSays.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.HideReturnsTransformationMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,11 +22,32 @@ public class SimonSays extends AppCompatActivity {
 
     }
 
+    public void Pressbuttonone(View v){
+        FindColor("blue", 0);
+        FloatingActionButton btn = (FloatingActionButton) findViewById(R.id.btnuser1);
+        btn.hide();
+    }
+    public void Pressbuttontwo(View v){
+        FindColor("blue", 0);
+
+    }
+    public void Pressbuttonthree(View v){
+        FindColor("blue", 0);
+
+    }
+    public void Pressbuttonfour(View v){
+        FindColor("blue", 0);
+
+    }
+    public void Pressbuttonfive(View v){
+        FindColor("blue", 0);
+
+    }
+
 
 
     public void Pressbuttonblue(View v){
-        Log.d("ID", "Test1");
-        UpdateButton("blue");
+        FindColor("blue", 0);
 
     }
     public void Pressbuttonyellow(View v){
@@ -40,32 +62,88 @@ public class SimonSays extends AppCompatActivity {
 
 
     }
-    public void UpdateButton(String Color){
-        Log.d("ID", "Test");
-        FloatingActionButton userone = (FloatingActionButton) findViewById(R.id.btnuser1);
-        FloatingActionButton usertwo = (FloatingActionButton) findViewById(R.id.btnuser2);
-        FloatingActionButton userthree = (FloatingActionButton) findViewById(R.id.btnuser3);
-        FloatingActionButton userfour = (FloatingActionButton) findViewById(R.id.btnuser4);
-        FloatingActionButton userfive = (FloatingActionButton) findViewById(R.id.btnuser5);
-        FloatingActionButton keyone = (FloatingActionButton) findViewById(R.id.btnkey1);
-        FloatingActionButton keytwo = (FloatingActionButton) findViewById(R.id.btnkey2);
-        FloatingActionButton keythree = (FloatingActionButton) findViewById(R.id.btnkey3);
-        FloatingActionButton keyfour = (FloatingActionButton) findViewById(R.id.btnkey4);
-        FloatingActionButton keyfive = (FloatingActionButton) findViewById(R.id.btnkey5);
+    public String FindColor(String Color, Integer Strikes) {
+        String Newcolor = "";
 
-        Log.d("ID", "Test");
-        if (userone.getVisibility() == View.VISIBLE){
-            keyone.setBackgroundColor(0xFFFFFFFF);
+        if (1 == 1) {
+            switch (Color) {
+                case "blue":
+                    if (Strikes == 0) {
+                        Newcolor = "red";
+                    } else if (Strikes == 1) {
+                        Newcolor = "green";
+                    } else if (Strikes == 2) {
+                        Newcolor = "red";
+                    }
+                    break;
+                case "yellow":
+                    if (Strikes == 0) {
+                        Newcolor = "green";
+                    } else if (Strikes == 1) {
+                        Newcolor = "red";
+                    } else if (Strikes == 2) {
+                        Newcolor = "blue";
+                    }
+                    break;
+                case "green":
+                    if (Strikes == 0) {
+                        Newcolor = "yellow";
+                    } else if (Strikes == 1) {
+                        Newcolor = "blue";
+                    } else if (Strikes == 2) {
+                        Newcolor = "yellow";
+                    }
+                    break;
+                case "red":
+                    if (Strikes == 0) {
+                        Newcolor = "blue";
+                    } else if (Strikes == 1) {
+                        Newcolor = "yellow";
+                    } else if (Strikes == 2) {
+                        Newcolor = "green";
+                    }
+                    break;
+            }
+        } else {
+            switch (Color) {
+                case "blue":
+                    if (Strikes == 0) {
+                        Newcolor = "yellow";
+                    } else if (Strikes == 1) {
+                        Newcolor = "blue";
+                    } else if (Strikes == 2) {
+                        Newcolor = "green";
+                    }
+                    break;
+                case "yellow":
+                    if (Strikes == 0) {
+                        Newcolor = "red";
+                    } else if (Strikes == 1) {
+                        Newcolor = "green";
+                    } else if (Strikes == 2) {
+                        Newcolor = "red";
+                    }
+                    break;
+                case "green":
+                    if (Strikes == 0) {
+                        Newcolor = "green";
+                    } else if (Strikes == 1) {
+                        Newcolor = "yellow";
+                    } else if (Strikes == 2) {
+                        Newcolor = "blue";
+                    }
+                    break;
+                case "red":
+                    if (Strikes == 0) {
+                        Newcolor = "blue";
+                    } else if (Strikes == 1) {
+                        Newcolor = "red";
+                    } else if (Strikes == 2) {
+                        Newcolor = "yellow";
+                    }
+                    break;
+            }
         }
-
-
-
-
-
-
-
-
+        return Newcolor;
     }
-
-
 }

4b0ab0f481e449803033988ba8db14e3283d076f
tmking01 <tmking01@comcast.net>
2017-07-23 00:49:05 -0500

    playing with the new homescreen

--- app/src/main/java/com/example/tmkin/keeptalkingandnobodyexplodescompanion/MainActivity.java
+++ app/src/main/java/com/example/tmkin/keeptalkingandnobodyexplodescompanion/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.tmkin.keeptalkingandnobodyexplodescompanion;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +21,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
+
+        final ImageAdapter org = new ImageAdapter(this);
+
+        final GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(org);
+
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                //Toast.makeText(gridview.this, "" + position,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick (AdapterView < ? > parent, View v,
+                                     int position, long id){
+
+                //Toast.makeText(getApplicationContext(), "" + position,Toast.LENGTH_SHORT).show();
+
+                ImageView imageView = (ImageView) v;
+            }
+        });
     }
 
     public void onbuttontap(View v) {
@@ -57,12 +86,83 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SimonSays.class);
         startActivity(intent);
     }
-
-
     public void openBombDefuse(View view){
         Uri defuseLink = Uri.parse("http://www.bombmanual.com/manual/1/html/index.html");
         Intent intent = new Intent(Intent.ACTION_VIEW, defuseLink);
         startActivity(intent);
     }
+
+
+
+
+
+
+
+
+    class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(215, 215));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(0, 0, 0, 0);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            String name = getResources().getResourceEntryName(mThumbIds[position]);
+            imageView.setTag(name);
+            imageView.setImageResource(mThumbIds[position]);
+
+
+
+            return imageView;
+        }
+
+        // references to our images
+        private Integer[] mThumbIds = {
+                R.drawable.complexwires
+                ,R.drawable.simplewires
+                ,R.drawable.mazes
+                ,R.drawable.pushbutton
+                ,R.drawable.memory
+                ,R.drawable.whoisonfirst
+                ,R.drawable.morsecode
+                ,R.drawable.simonsays
+                ,R.drawable.password
+                ,R.drawable.symbols
+                ,R.drawable.bombdefusalmanual
+                ,R.drawable.wiresequence
+        };
+
+
+
+    }
+
+
+
+
 }
 
